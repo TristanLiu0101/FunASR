170f2688de20e0273079d5bf725a10daef5eda6e
TristanLiu0101 <lzc20020101@163.com>
2023-04-07 19:25:54 +0800

    change: outputs_dir and inputs_dir in infer.py

--- egs_modelscope/asr_vad_punc/speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-pytorch/infer.py
+++ egs_modelscope/asr_vad_punc/speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-pytorch/infer.py
@@ -2,8 +2,9 @@ from modelscope.pipelines import pipeline
 from modelscope.utils.constant import Tasks
 
 if __name__ == '__main__':
-    audio_in = 'https://isv-data.oss-cn-hangzhou.aliyuncs.com/ics/MaaS/ASR/test_audio/asr_example_zh.wav'
-    output_dir = None
+    # audio_in = 'https://isv-data.oss-cn-hangzhou.aliyuncs.com/ics/MaaS/ASR/test_audio/asr_example_zh.wav'
+    audio_in = "C:/Users/liuzhechen/Desktop/S0002/SCP/wav.scp"
+    output_dir = "C:/Users/liuzhechen/Desktop/S0002/outputs_v032"
     inference_pipeline = pipeline(
         task=Tasks.auto_speech_recognition,
         model='damo/speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-pytorch',
@@ -13,6 +14,7 @@ if __name__ == '__main__':
         punc_model='damo/punc_ct-transformer_zh-cn-common-vocab272727-pytorch',
         punc_model_revision="v1.1.6",
         ngpu=1,
+        output_dir = output_dir,
     )
     rec_result = inference_pipeline(audio_in=audio_in)
     print(rec_result)
